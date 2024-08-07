10c7d2ef1f2f664bba20ca3899447a63e209e914
hplt <hplt@mon003-cl3-mka-ova1.du.cesnet.cz>
2024-08-08 01:17:32 +0200

    removed unsupported option

--- run_aria.sh
+++ run_aria.sh
@@ -1,3 +1,2 @@
 FILELIST=$1
-OUTDIR=$2
-aria2c -i $FILELIST  --max-tries=150 -d $OUTDIR -l ${FILELIST}_log --max-file-not-found=150 -V --save-session=$FILELIST -j 100 --retry-wait=3
+aria2c -i $FILELIST  --max-tries=150 -l ${FILELIST}_log --max-file-not-found=150 -V --save-session=$FILELIST -j 100 --retry-wait=3

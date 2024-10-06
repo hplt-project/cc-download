cde069397c3e036ace61536ae4c806e475232ed0
hplt <hplt@mon003-cl3-mka-ova1.du.cesnet.cz>
2024-10-06 15:53:39 +0200

    switched to https from http for CC downloading; all crawls are split into portions having 10 random crawls

--- run_aria.sh
+++ run_aria.sh
@@ -1,2 +1,3 @@
 FILELIST=$1
-aria2c -i $FILELIST  --max-tries=150 -l ${FILELIST}_log --max-file-not-found=150 -V --save-session=$FILELIST -j 100 --retry-wait=3
+NTHREADS=$2
+aria2c -i $FILELIST  --max-tries=150 -l ${FILELIST}_log --log-level=notice --max-file-not-found=150 -V --save-session=$FILELIST -j $NTHREADS --retry-wait=1

553da869fc8715614ce58c605c3327e71bb0fac7
Nikolay Arefev <nikolare@hplt.nird.hplt-ns8112k.svc.cluster.local>
2024-12-17 17:47:17 +0100

    downloading to segment-specific folders

--- generate_aria_input.sh
+++ generate_aria_input.sh
@@ -2,4 +2,4 @@ CC_PATHS=$1
 OUT_DIR=$(basename "$CC_PATHS")
 OUT_DIR="${OUT_DIR%.*}"
 mkdir -p  $OUT_DIR
-for x in `cat $CC_PATHS`; do zcat warcpaths/$x | sed -r 's!(.*)$!https://data.commoncrawl.org/\1\t  dir='${OUT_DIR}/$(basename $x .gz)'!'; done |shuf|tr '\t' '\n'
+for x in `cat $CC_PATHS`; do zcat warcpaths/$x | sed -r 's!((.*)/[^/]+)$!https://data.commoncrawl.org/\1\t  dir='${OUT_DIR}/'\2!'; done |shuf|tr '\t' '\n'

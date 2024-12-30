b80641341cd68d422bc562d3a8d175ce17566b7c
Nikolay Arefev <nikolare@hplt.nird.hplt-ns8112k.svc.cluster.local>
2024-12-30 16:06:24 +0100

    getting crawl sizes from CC website

--- get_crawl_sizes.sh
+++ get_crawl_sizes.sh
@@ -1,2 +1,5 @@
-crawl=CC-MAIN-2024-51
-(echo $crawl; curl -s https://data.commoncrawl.org/crawl-data/${crawl}/index.html|grep -A 2 warc.paths.gz|tail -2|sed -r 's!.*>([0-9\.]+)<.*!\1!')|tr '\n' '\t'; echo ''
+LIST=$1
+(for crawl in `sed 's!\.gz!!' $LIST`; do
+  (echo $crawl; curl -s https://data.commoncrawl.org/crawl-data/${crawl}/index.html|grep -A 2 warc.paths.gz|tail -2|sed -r 's!.*>([0-9\.]+)<.*!\1!')|tr '\n' '\t'; echo ''
+done) |tee ${LIST}.sizes
+cut -f 3 ${LIST}.sizes | python -c 'import sys; print(sum(float(l) for l in sys.stdin))'
